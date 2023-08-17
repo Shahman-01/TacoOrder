4df0ef726b0150cf99c4edf0fbd0eee5f42e7677
Shahman Magomadov <magomadovshahman@gmail.com>
2023-08-17 19:38:53 +0300

    started SpringInAction project

---
+++ src/test/java/tacos/TacoCloudApplicationTest.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TacoCloudApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+
+}
