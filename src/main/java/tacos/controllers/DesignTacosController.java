f57463277335046417a090df0a9479a9569f73d9
Shahman Magomadov <magomadovshahman@gmail.com>
2023-08-19 22:07:20 +0300

    add convertor

--- src/main/java/tacos/controllers/DesignTacosController.java
+++ src/main/java/tacos/controllers/DesignTacosController.java
@@ -3,10 +3,7 @@ package tacos.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import tacos.models.Ingredient;
 import tacos.models.Ingredient.Type;
 import tacos.models.Taco;
@@ -58,6 +55,15 @@ public class DesignTacosController {
 	@GetMapping
 	public String showDesignForm() {
 		return "design";
+	}
+
+	@PostMapping
+	public String processTaco(Taco taco,
+	                          @ModelAttribute TacoOrder tacoOrder) {
+		tacoOrder.addTaco(taco);
+		log.info("Processing taco: {}", taco);
+
+		return "redirect:/orders/current";
 	}
 
 	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
