61a223a15d0fd42b3268674adc7b90313c4cd4e8
Shahman Magomadov <magomadovshahman@gmail.com>
2023-09-11 11:18:38 +0300

    mod TacoOrder

--- src/main/java/tacos/models/TacoOrder.java
+++ src/main/java/tacos/models/TacoOrder.java
@@ -1,7 +1,6 @@
 package tacos.models;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.CreditCardNumber;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
@@ -34,10 +33,10 @@ public class TacoOrder implements Serializable {
 	@NotBlank(message = "Zip code is required")
 	private String deliveryZip;
 
-	@CreditCardNumber(message = "Not a valid credit card number")
+	@NotBlank(message = "Not a valid credit card number")
 	private String ccNumber;
 
-	@Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
+	@Pattern(regexp = "^(0[1-9]|1[0-2])(/)([0-9][0-9])$", message = "Must be formatted MM.YY")
 	private String ccExpiration;
 
 	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
