da81f58717c48a53b59e5a974a368d070b0b2d32
Shahman Magomadov <magomadovshahman@gmail.com>
2023-09-12 08:58:22 +0300

    write jpa realization

--- src/main/java/tacos/dao/OrderRepository.java
+++ src/main/java/tacos/dao/OrderRepository.java
@@ -3,5 +3,11 @@ package tacos.dao;
 import org.springframework.data.repository.CrudRepository;
 import tacos.models.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+	List<TacoOrder> findByDeliveryZip(String deliveryZip);
+	List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+			String deliveryZip, Date startDate, Date endDate);
 }
--- src/main/java/tacos/models/TacoOrder.java
+++ src/main/java/tacos/models/TacoOrder.java
@@ -1,9 +1,8 @@
 package tacos.models;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -13,11 +12,12 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class TacoOrder implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	private Date placedAt;
@@ -46,6 +46,7 @@ public class TacoOrder implements Serializable {
 	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
 	private String ccCVV;
 
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Taco> tacos = new ArrayList<>();
 
 	public void addTaco(Taco taco) {
