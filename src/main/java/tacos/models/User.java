fa2be59b2c78a8fc605f492ee322ee2012db8886
Shahman Magomadov <magomadovshahman@gmail.com>
2023-09-25 08:01:47 +0300

    start making security

--- src/main/java/tacos/models/User.java
+++ src/main/java/tacos/models/User.java
@@ -1,8 +1,11 @@
 package tacos.models;
 
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
@@ -10,29 +13,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Collection;
+import java.util.Collections;
 
 @Entity
-@NoArgsConstructor
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
 public class User implements UserDetails {
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	private String username;
-	private String password;
-	private String fullname;
-	private String street;
-	private String city;
-	private String state;
-	private String zip;
-	private String phoneNumber;
+	private final String username;
+	private final String password;
+	private final String fullname;
+	private final String street;
+	private final String city;
+	private final String state;
+	private final String zip;
+	private final String phoneNumber;
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
 	}
 
 	@Override
