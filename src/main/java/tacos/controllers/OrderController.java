a119988cbfa89bd4350277b59fe794f810f155f1
Shahman Magomadov <magomadovshahman@gmail.com>
2023-09-27 09:08:57 +0300

    mod OrderController

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -1,8 +1,11 @@
 package tacos.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,15 +18,22 @@ import tacos.models.TacoOrder;
 import tacos.models.User;
 
 import javax.validation.Valid;
+import java.awt.print.Pageable;
 
 @Controller
 @Slf4j
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
 	private final OrderRepository orderRepository;
 	private final JpaUserRepository userRepository;
+	private int pageSize = 20;
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
 
 	public OrderController(OrderRepository orderRepository, JpaUserRepository userRepository) {
 		this.orderRepository = orderRepository;
@@ -33,6 +43,15 @@ public class OrderController {
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
+	}
+
+	@GetMapping
+	public String ordersForUser(
+			@AuthenticationPrincipal User user, Model model) {
+		Pageable pageable = (Pageable) PageRequest.of(0, pageSize);
+		model.addAttribute("orders",
+				orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
 	}
 
 	@PostMapping
--- src/main/java/tacos/dao/OrderRepository.java
+++ src/main/java/tacos/dao/OrderRepository.java
@@ -2,7 +2,9 @@ package tacos.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import tacos.models.TacoOrder;
+import tacos.models.User;
 
+import java.awt.print.Pageable;
 import java.util.Date;
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 	List<TacoOrder> findByDeliveryZip(String deliveryZip);
 	List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
 			String deliveryZip, Date startDate, Date endDate);
+
+	List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
