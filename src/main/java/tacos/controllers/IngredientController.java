0b8f105caa29a5d84b0bdf7e869c02078c14ecc8
Shahman Magomadov <magomadovshahman@gmail.com>
2023-10-10 09:17:49 +0300

    add jwt auth

--- src/main/java/tacos/controllers/IngredientController.java
+++ src/main/java/tacos/controllers/IngredientController.java
@@ -2,6 +2,7 @@ package tacos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tacos.dao.IngredientRepository;
 import tacos.models.Ingredient;
@@ -23,12 +24,14 @@ public class IngredientController {
 	}
 
 	@PostMapping
+	@PreAuthorize("#{hasRole('ADMIN')}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
 		return repo.save(ingredient);
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("#{hasRole('ADMIN')}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteIngredient(@PathVariable("id") String id) {
 		repo.deleteById(id);
--- src/main/java/tacos/dao/JpaUserRepository.java
+++ src/main/java/tacos/dao/JpaUserRepository.java
@@ -1,8 +1,10 @@
 package tacos.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tacos.models.User;
 
+@Repository
 public interface JpaUserRepository extends CrudRepository<User, Long> {
 
 	User findByUsername(String username);
